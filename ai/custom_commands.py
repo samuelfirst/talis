b8d96c6ca97ab7fad1e0a8bf0f7ddbdbe1f281c0
bas <basilieus@gmail.com>
2019-01-27 21:34:57 -0800

    Consolidate AI/Spam and CustomCommands

---
+++ ai/custom_commands.py
@@ -0,0 +1,25 @@
+'''
+Use this command to attach custom rules based commands to the bot
+
+!git -> (url)
+!bot -> (description)
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+from talis.config import config
+from talis.log import log
+from talis.kafka.stdout_consumer import StdoutConsumer
+
+if __name__ == "__main__":
+    try:
+        consumer = StdoutConsumer(
+            topic=config.get('topic'),
+            bootstrap_servers=config.get('KAFKA_BOOTSTRAP_HOST'),
+            auto_offset_reset=config.get('auto_offset_reset', 'latest')
+        )
+        consumer.start()
+    except:
+        exit(".\n")
+
+'''
---
+++ talis/kafka/spam_detector_consumer.py
@@ -0,0 +1,73 @@
+import time
+import collections
+
+from .queue_consumer import QueueCosumer
+
+from ..log import log
+
+# fix
+def log_info(msg):
+    print("AI SPAM: {}".format(msg))
+
+class SpamDetectorConsumer(QueueCosumer):
+    def __init__(self, minimum_population, unique_threshold, distribution_length_ms,
+            queue, stop_event, *args, **kwargs):
+        super().__init__(queue, stop_event, *args, **kwargs)
+        self.message_bin = []
+        self.minimum_population = minimum_population
+        self.unique_threshold = unique_threshold
+        self.distribution_length_ms = distribution_length_ms
+        self.force_reset = False
+        self.start_time = time.time()
+
+    def calculate_unique_distribution(self):
+        bin_len = len(self.message_bin)
+        counter = collections.Counter(map(lambda x : x.lower(), self.message_bin))
+        most_common_count = counter.most_common(1)[0][1]
+        r = most_common_count/bin_len
+        log_info("{0:.2f}% of {1:.2f}% threshold".format(r*100, unique_threshold*100))
+        return r
+
+    def collapse_message(self, msg):
+        m_split = msg.decode('utf-8').lower().split(" ")
+        bin_len = len(m_split)
+        unique = len(list(set(map(lambda x : x.lower(),m_split))))
+        if bin_len > 1 and unique == 1:
+            log_info("DUPLICATE SPAM {0}".format(msg, m_split[0]))
+            return bytes(m_split[0], 'utf-8')
+        else:
+            return msg
+
+    def send_bot_message(self):
+        counter = collections.Counter(self.message_bin)
+        msg = counter.most_common(1)[0][0]
+        try:
+            log_info("====== SPAM TRIGGER ======".format(msg))
+            log_info("{0}".format(msg))
+            log_info("====== END TRIGGER ======".format(msg))
+            self.queue.put_nowait(msg)
+        except:
+            raise
+
+    def run(self):
+        while not self.stop_event.is_set():
+            for msg in self.consumer:
+                if self.force_reset:
+                    self.force_reset = False
+                    self.message_bin = []
+                    self.start_time = time.time()
+                end_time = time.time()
+                diff = end_time - self.start_time
+                if len(self.message_bin) < self.minimum_population:
+                    log_info("accumulating bin {0:.2f}/seconds".format(diff))
+                self.message_bin.append(self.collapse_message(msg.value))
+                unique_perc = self.calculate_unique_distribution()
+                if len(self.message_bin) > self.minimum_population:
+                    self.message_bin.pop(0)
+                    if unique_perc > self.unique_threshold:
+                        self.force_reset = True
+                        if diff > self.distribution_length_ms:
+                            self.send_bot_message()
+                self.processed += 1
+            if self.stop_event.is_set():
+                break
