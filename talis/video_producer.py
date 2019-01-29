97db25ad97a41e3f54c17717fafc896655c03f56
bas <basilieus@gmail.com>
2019-01-29 05:10:36 -0800

    Fix naming convention and typos for recording clips

--- talis/video_producer.py
+++ talis/video_producer.py
@@ -1,11 +1,8 @@
-# TODO: Format execution string
-# naming is a bit weird "producer" mixes with Kafka Producer
-
 import threading
-
 import os
 
 from talis import TalisQueue
+from talis import log
 
 class VideoProducer(threading.Thread, TalisQueue):
 
@@ -13,18 +10,16 @@ class VideoProducer(threading.Thread, TalisQueue):
         threading.Thread.__init__(self)
         TalisQueue.__init__(self, queue)
 
-    # ENTRY POINT FOR THREAD
     def run(self):
         while True:
             try:
-                # BLOCKING and THREAD LOCKING
                 data = self.queue.get()
             except:
                 break
             try:
                 channel = data.get('channel')
-                print('getting clip of {}'.format(channel))
-                os.system("cd ~/sites/talis && source env/bin/activate && nohup python scripts/record_video.py https://www.twitch.tv/{0} {1} & disown".format(channel, channel))
+                log.info('getting clip of {}'.format(channel))
+                os.system("cd ~/sites/talis && source env/bin/activate && nohup python scripts/record_clip.py https://www.twitch.tv/{0} {1} & disown".format(channel, channel))
             except:
                 pass
             self.queue.task_done()
